1a56d41417c7178cfaea9346c226249a01a14257
erdogan <eerdogan.github@gmail.com>
2022-04-12 02:43:58 +0300

    django kurulum

---
+++ DjangoTuristikMekan/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DjangoTuristikMekan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoTuristikMekan.settings')
+
+application = get_wsgi_application()
