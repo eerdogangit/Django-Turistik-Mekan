1c8e924a6f7adb68487a25802b69ebbeffaa1aa7
erdogan <eerdogan.github@gmail.com>
2022-05-18 02:44:35 +0300

    video4-5-6-7

--- place/admin.py
+++ place/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from place.models import Category, Place, Sehir, Ulke, Images
+class PlaceImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+admin.site.register(Category, CategoryAdmin)
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status']
+    list_filter = ['status', 'category']
+    inlines = [PlaceImageInline]
+admin.site.register(Place, PlaceAdmin)
+
+class SehirAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+admin.site.register(Sehir, SehirAdmin)
+
+class UlkeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+admin.site.register(Ulke, UlkeAdmin)
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'place', 'image']
+admin.site.register(Images, ImagesAdmin)
+
--- place/models.py
+++ place/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -20,3 +21,70 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+class Sehir(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS)
+
+    def __str__(self):
+        return self.title
+
+class Ulke(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS)
+
+    def __str__(self):
+        return self.title
+
+class Place(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # sonradan ekledim
+    title = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='placeimages/')
+    detail = models.TextField()
+    slug = models.SlugField(blank=True)
+
+    sehir = models.ForeignKey(Sehir, on_delete=models.CASCADE, blank=True, null=True)
+    ulke = models.ForeignKey(Ulke, on_delete=models.CASCADE, blank=True, null=True)
+
+    adres = models.CharField(max_length=255, blank=True)
+    telefon = models.CharField(max_length=255, blank=True)
+    email = models.CharField(blank=True, max_length=255)
+    facebook = models.CharField(blank=True, max_length=255)
+    twitter = models.CharField(blank=True, max_length=255)
+    instagram = models.CharField(blank=True, max_length=255)
+
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Images(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(blank=True, upload_to='galimages/')
+
+    def __str__(self):
+        return self.title
+
+    #def image_tag(self):
+    #    return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    #image_tag.short_description = 'Image'
