81157cada324d8c8fdfc53dd8be38fd41211db1d
erdogan <eerdogan.github@gmail.com>
2022-05-25 18:43:28 +0300

    25-uye-profile-ekbilgiler-resim

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from home.models import Setting, ContactFormMessage
+from home.models import Setting, ContactFormMessage, Profile, Faq
 
 class ContactFormMessageAdmin(admin.ModelAdmin):
     list_display = ['firstname', 'lastname', 'email', 'message', 'status']
     list_filter = ['status']
 admin.site.register(ContactFormMessage, ContactFormMessageAdmin)
 admin.site.register(Setting)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'image_tag', 'phone', 'address']
+admin.site.register(Profile, ProfileAdmin)
+
+class FaqAdmin(admin.ModelAdmin):
+    list_display = ['question', 'answer', 'status']
+    list_filter = ['status']
+admin.site.register(Faq,FaqAdmin)
